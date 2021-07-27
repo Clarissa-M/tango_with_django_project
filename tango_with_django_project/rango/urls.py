0b264fca5cb871e99f8b2ed04df414a4ca92a812
Clarissa Wilkie <2515325w@student.gla.ac.uk>
2021-07-27 10:13:32 +0100

    Chapter3 tested

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from rango import views
 
-app_name = 'rango'
+app_name = "rango"
 
 urlpatterns =[
+    path('about/', views.about, name='about'),
     path('', views.index, name='index'),
+    
 ]
