0dd589c82e182c2e92bf6ca25f5a1969bcf972d0
Clarissa Wilkie <2515325w@student.gla.ac.uk>
2021-07-28 17:33:03 +0100

    Chapter5 test successful

--- tango_with_django_project/populate_rango.py
+++ tango_with_django_project/populate_rango.py
@@ -14,11 +14,11 @@ def populate():
         'url':'http://docs.python.org/3/tutorial/'},
         {'title': 'How to Think like a Computer Scientist',
         'url': 'http://www.greenteapress.comTthinkpython/'},
-        {'title': 'Learn python in 10 Minutes',
+        {'title': 'Learn Python in 10 Minutes',
         'url': 'http://www.korokithakis.net/tutorials/python/'}]
 
     django_pages = [
-        {'title': 'Official Python Tutorial',
+        {'title': 'Official Django Tutorial',
         'url':'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
         {'title': 'Django Rocks',
         'url': 'http://www.djangorocks.com/'},
