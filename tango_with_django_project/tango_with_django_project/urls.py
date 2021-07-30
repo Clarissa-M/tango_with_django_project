aef2794f9078434b41032bb97ac35bbef7bd46ac
Clarissa Wilkie <2515325w@student.gla.ac.uk>
2021-07-30 09:16:08 +0100

    Chapter 9

--- tango_with_django_project/tango_with_django_project/urls.py
+++ tango_with_django_project/tango_with_django_project/urls.py
@@ -25,4 +25,7 @@ urlpatterns = [
     path('rango/', include('rango.urls')),
     # the above maps any URLs starting with rango/ to be handled by rango.
     path('admin/', admin.site.urls),
+    
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
